bda41856d23bb4576ac9458df02506087fc6525f
Desiree Ann Guzman <desireeann000@gmail.com>
2024-09-10 20:03:45 +0800

    Completed Comparison, Relational and Logical Operators section

---
+++ comparison.js
@@ -0,0 +1,36 @@
+let firstFavNumb = 12;
+let secondFavNumb = 15; 
+
+// GREATER THAN
+console.log("Is 12 greater than 15? ", firstFavNumb > secondFavNumb); 
+
+// LESS THAN
+console.log("Is 12 less than 15? ", firstFavNumb < secondFavNumb); 
+
+// GREATER THAN OR EQUAL TO
+console.log("Is 12 greater than or equal to 15? ", firstFavNumb >= secondFavNumb);
+
+// LESS THAN OR EQUAL TO
+console.log("Is 12 less than or equal to 15? ", firstFavNumb <= secondFavNumb);
+
+// STRICT EQUALITY (===)
+console.log("Strict equality (firstFavNumb === secondFavNumb): ", firstFavNumb === secondFavNumb);
+
+// LOOSE EQUALITY (==)
+console.log("Loose equality (firstFavNumb == secondFavNumb): ", firstFavNumb == secondFavNumb);
+
+// STRICT INEQUALITY (!==)
+console.log("Strict inequality (firstFavNumb !== secondFavNumb): ", firstFavNumb !== secondFavNumb);
+
+// LOOSE INEQUALITY (!=)
+console.log("Loose inequality (firstFavNumb != secondFavNumb): ", firstFavNumb != secondFavNumb);
+
+// AND (&&)
+console.log("AND (firstFavNumb > 0 && secondFavNumb > 0): ", firstFavNumb > 0 && secondFavNumb > 0);
+
+// OR (||)
+console.log("OR (firstFavNumb > 0 || secondFavNumb < 0): ", firstFavNumb > 0 || secondFavNumb < 0);
+
+// NOT (!)
+console.log("NOT (!(firstFavNumb < 0)): ", !(firstFavNumb < 0));
+
